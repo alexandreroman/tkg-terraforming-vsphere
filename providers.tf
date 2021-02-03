539969bd0c1ae45fddccdc881d2ac3cd5ed79a27
Alexandre Roman <aroman@vmware.com>
2021-02-03 15:11:09 +0100

    Fix Terraform configuration

--- providers.tf
+++ providers.tf
@@ -1,14 +1,15 @@
 # Make sure you lock down all providers version.
 
-provider "vsphere" {
-  version = "~> 1.16"
+terraform {
+  required_providers {
+    vsphere = "~> 1.16"
+    local = "~> 1.4"
+  }
+}
 
+provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
   vsphere_server       = var.vsphere_server
   allow_unverified_ssl = true
-}
-
-provider "local" {
-  version = "~> 1.4"
 }
